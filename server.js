649ea364b2765d2f80dd51794396f0949f009f9e
costaleonardo <costaleonardo97@gmail.com>
2018-09-05 01:06:35 -0400

    Add validation handlers.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 // Routes
 const users = require('./routes/api/users');
@@ -29,7 +30,11 @@ mongoose
   .then(() => console.log('MongoDB connected.'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
